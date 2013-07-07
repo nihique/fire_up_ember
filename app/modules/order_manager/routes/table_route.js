a8d37cebc4ea6a2cd30187ce54fa9deda84caae2
nihique <kovarik.martin@gmail.com>
2013-07-07 22:31:22 +0200

    refactoring + styling

--- app/modules/order_manager/routes/table_route.js
+++ app/modules/order_manager/routes/table_route.js
@@ -1,7 +1,4 @@
 FireUpEmber.TableRoute = Ember.Route.extend({
-    model: function (params) {
-        return FireUpEmber.Table.find(params.table_id);
-    },
 
     renderTemplate: function () {
         this.render('order_manager/table', {
--- app/router.js
+++ app/router.js
@@ -1,6 +1,6 @@
 FireUpEmber.Router.map(function () {
     this.resource('order_manager', function () {
-        this.resource('tables', { path: 'tables' }, function () {
+        this.resource('tables', { path: 'order_manager/tables' }, function () {
             this.resource('table', { path: ':table_id' });
         });
     });
